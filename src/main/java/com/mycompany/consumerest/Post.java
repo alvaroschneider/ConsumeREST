92f2517a3ae75c9bc993d19622c473e7ada89e00
alvaro.schneider <alvaro.c.schneider@gmail.com>
2022-05-09 11:24:37 -0300

    Inicializamos el repo con las clases básicas.

---
+++ src/main/java/com/mycompany/consumerest/Post.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.consumerest;
+
+import java.io.Serializable;
+
+/**
+ *  "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+ * @author alvaro
+ */
+public class Post implements Serializable{
+    private Integer Id;
+    private Integer userId;
+    private String title;
+    private String body;
+
+    public Integer getId() {
+        return Id;
+    }
+
+    public void setId(Integer Id) {
+        this.Id = Id;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+    
+}
